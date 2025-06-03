f6e97c8afd287f2536b672bff862022dc119f2c7
Elmyk <161933905+Elmyk@users.noreply.github.com>
2025-06-03 17:38:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Change header color on click
+document.querySelector('header').addEventListener('click', function() {
+    this.style.backgroundColor = '#ff5733';
+});
+
+// You can add more dynamic features here
